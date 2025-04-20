a8c4638ead05865d78dba7146d3bccc7ab7d6fb1
kana <156772506+kana542@users.noreply.github.com>
2025-04-20 21:08:14 +0300

    finishing touches

--- tests/asennustesti.py
+++ tests/asennustesti.py
@@ -6,18 +6,15 @@ muiden käytettyjen kirjastojen versiot. Tämä tiedosto on tarkoitettu
 käytettäväksi testattaessa, että kaikki tarvittavat kirjastot on asennettu.
 """
 
-# Tulostetaan Pythonin versio
 import sys
 print('Python:', sys.version)
 
-# Tulostetaan Robot Frameworkin ja muiden käytettyjen kirjastojen versiot
 try:
     import robot
     print('Robot Framework:', robot.__version__)
 except ImportError:
     print('robot-moduulia ei löydy')
 
-# Tarkistetaan, onko käytössä Browser-, RequestsLibrary- ja CryptoLibrary-moduulit
 try:
     import Browser
     print('Browser:', Browser.__version__)
@@ -25,7 +22,6 @@ except ImportError:
     print('Browser-moduulia ei löydy')
 
 try:
-    # RequestsLibrary on saatavilla Pythonsissa nimellä requests
     import requests
     print('requests:', requests.__version__)
 except ImportError:
